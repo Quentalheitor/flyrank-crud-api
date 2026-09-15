fd9cf129a5ff09c61178029c1075cbd61d110d6b
Heitor Quental <heitorquental321@gmail.com>
2026-09-14 21:27:14 -0300

    Stage 2: read endpoints with 404

--- python-fastapi/main.py
+++ python-fastapi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 
 app = FastAPI()
@@ -14,6 +14,23 @@ def hello():
 @app.get("/health")
 def status():
     return {"status": "ok"}
+
+lista = {
+    "task1": {"id": "01", "title": "title1", "done": "done1"},
+    "task2": {"id": "02", "title": "title2", "done": "done2"},
+    "task3": {"id": "03", "title": "title3", "done": "done3"}
+}
+
+@app.get("/tasks")
+def tasklist():
+    return lista
+
+@app.get("/tasks/{id}")
+def get_single_tsk(id: str):
+    for task in lista.values():
+        if task["id"] == id:
+            return task
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
